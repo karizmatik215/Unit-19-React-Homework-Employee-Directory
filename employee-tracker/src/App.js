6d9f798dbb795753a8bb5f7f1d4f38a512ba1a93
Juan Sanchez <juan.sanchez@phila.gov>
2021-05-06 17:21:49 -0400

    initial App file with Wrapper and Header

---
+++ employee-tracker/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Wrapper from './components/Wrapper';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div>
+      <Wrapper>
+        <Header />
+      </Wrapper>
+    </div>
+  );
+}
+
+export default App;
